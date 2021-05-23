8f6722fa23720d70c4ee45bf074732005e6b5ba4
daiki130 <daikibaseball130@gmail.com>
2021-05-23 19:32:17 +0900

    ✨ 🎉 init commit

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vutify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos porro
+      debitis facilis quidem voluptate alias expedita quo, minus molestiae
+      neque? Quisquam ut architecto, adipisci nulla ratione quasi pariatur atque
+      suscipit?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos porro
+      debitis facilis quidem voluptate alias expedita quo, minus molestiae
+      neque? Quisquam ut architecto, adipisci nulla ratione quasi pariatur atque
+      suscipit?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos porro
+      debitis facilis quidem voluptate alias expedita quo, minus molestiae
+      neque? Quisquam ut architecto, adipisci nulla ratione quasi pariatur atque
+      suscipit?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos porro
+      debitis facilis quidem voluptate alias expedita quo, minus molestiae
+      neque? Quisquam ut architecto, adipisci nulla ratione quasi pariatur atque
+      suscipit?
+    </p>
+  </div>
+</template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <v-list flat three-line>
+      <v-list-item>
+        <template v-slot:default="{ active }">
+          <v-list-item-action>
+            <v-checkbox :input-value="active"></v-checkbox>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Notifications</v-list-item-title>
+            <v-list-item-subtitle
+              >Notify me about updates to apps or games that I
+              downloaded</v-list-item-subtitle
+            >
+          </v-list-item-content>
+        </template>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
